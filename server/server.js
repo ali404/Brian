04dd12a56ea9a6343fd802dab2e73294f4b0ef5a
ali404 <acerrahoglu.ali@gmail.com>
2017-04-04 15:55:00 +0100

    set up Node server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.set('port', (process.env.PORT || 3001))
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+
+app.listen(app.get('port'), function() {
+  console.log('Find the server at: http://localhost:3001/')
+})
